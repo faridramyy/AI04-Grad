56612f0f965d5aeb2ea1953938be046c1bc01fa7
imposterjump <125587220+imposterjump@users.noreply.github.com>
2025-03-18 02:26:26 +0200

    removed comments in the database script

--- backend/models/run_scripts.js
+++ backend/models/run_scripts.js
@@ -1,18 +1,18 @@
-// import AI_Message from "../models/ai_messages.js";
-// import User from "../models/user.js";
-// import TherapySession from "../models/therapy_session.js";
-// import UserFeedback from "../models/user_feedback.js";
-// import StressScenario from "../models/stress_scenario.js";
-// import GameSession from "../models/game_session.js";
-// import ExtractedEmotion from "../models/extracted_emotions.js";
-// import SevereCase from "../models/severe_case.js";
-// import UserSettings from "./user_settings.js";
-// //mongodb+srv://Rina:25102002b@rina.gp6gt.mongodb.net/rina_dataset/
-// mongoose.connect("mongodb+srv://Rina:25102002b@rina.gp6gt.mongodb.net/rina_dataset/", {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     }).then(() => console.log("MongoDB Connected"))
-//     .catch(err => console.log(err));
+import AI_Message from "../models/ai_messages.js";
+import User from "../models/user.js";
+import TherapySession from "../models/therapy_session.js";
+import UserFeedback from "../models/user_feedback.js";
+import StressScenario from "../models/stress_scenario.js";
+import GameSession from "../models/game_session.js";
+import ExtractedEmotion from "../models/extracted_emotions.js";
+import SevereCase from "../models/severe_case.js";
+import UserSettings from "./user_settings.js";
+//mongodb+srv://Rina:25102002b@rina.gp6gt.mongodb.net/rina_dataset/
+mongoose.connect("mongodb+srv://Rina:25102002b@rina.gp6gt.mongodb.net/rina_dataset/", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 // async function insertDummyAiMessages() {
 //     try {
