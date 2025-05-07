8fef1024bc617c475e498126f7be8ae2ab4d266b
farid ramy <faridramy2003@gmail.com>
2025-05-07 15:59:04 +0300

    added the history panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Canvas } from "@react-three/fiber";
 import { Leva } from "leva";
 import { Experience } from "./components/Experience";
 import { UI } from "./components/UI";
+import FloatingHistoryPanel from "./components/FloatingHistoryPanel";
 
 function App() {
   return (
     <>
       <Loader />
       <Leva hidden />
+      <FloatingHistoryPanel />
       <UI />
       <Canvas shadows camera={{ position: [0, 0, 1], fov: 30 }}>
         <Experience />
